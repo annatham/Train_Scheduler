1099cd3ff0153e92f3d71e339548b61ed73b838b
Anna Thammakane <anna.thammakane@gmail.com>
2019-09-09 01:31:16 -0500

    color

---
+++ javascript/code.js
@@ -0,0 +1,15 @@
+
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+var config = {
+    // apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
+    // authDomain: "time-sheet-55009.firebaseapp.com",
+    // databaseURL: "https://time-sheet-55009.firebaseio.com",
+    // storageBucket: "time-sheet-55009.appspot.com"
+  };
